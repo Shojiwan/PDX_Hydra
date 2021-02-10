ee53c3c6c4d93a182cf79e3bb6cdc953880fc129
Shojiwan <74939573+Shojiwan@users.noreply.github.com>
2021-02-09 19:26:28 -0800

    Updated read_data function

--- dash_app/python/hydra_fncs.py
+++ dash_app/python/hydra_fncs.py
@@ -1,19 +1,48 @@
+#2345678901234567890123456789012345678901234567890123456789012345678901234567890
+
+import urllib.request
+import pandas as pd
+import re
+from io import StringIO
+
 def read_data(stID, strD, endD):
 
-  # Read data from station for start date to end date
-  
-  import urllib
+  # Function to read data from HYDRA station for start date to end date
 
   # Look up URL from station ID
-  link = "https://or.water.usgs.gov/non-usgs/bes/hayden_island.rain"
-
-  f = urllib.request(link)
-
-  myfile = f.read()
   
-  print(myfile)
 
-  # Read the urls
+  # Read the urls  
+  link = "https://or.water.usgs.gov/non-usgs/bes/open_meadows.rain"
+
+  f = urllib.request.urlopen(link)
+
+  pDat = str(f.read())
+
+  # Split header text, header line and data
+  pDat = re.split("Daily  Hourly data -->\\\\n   |-{114}\\\\n", pDat)
+  
+  # For the header line, insert an "H" in front of the number
+  pDat = re.sub('\s+', ' H', pDat[1]) + pDat[2]
+
+  # Fix the carriage returns
+  pDat = re.sub('\\\\n', '\\n', pDat)
+
+  pDat = StringIO(pDat)
+
+  pDat = pd.read_csv(pDat, sep = "\s+")
+
+  # pDat.to_csv('C:/Users/ryans/Desktop/RMS/002_projects/pdx_hydra/dash_app/data/test.csv',index = False)
+  
+  return(pDat)
+
+
+
+
+
+
+
+
   
   # Remove the first entry of the URL file (column name)
   
